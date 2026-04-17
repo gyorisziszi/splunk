5919b232d20569cb4fd9f3c30a919e21e31442f3
gyorisziszi <gyorisziszi@gmail.com>
2026-04-17 23:21:31 +0200

    Update install_splunk_ent.sh

--- install_splunk_ent.sh
+++ install_splunk_ent.sh
@@ -34,5 +34,4 @@ chown -R splunk:splunk /opt/splunk
 sudo -u $SPLUNK_LINUXUSER $SPLUNK_HOME/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd admin123
 
 $SPLUNK_HOME/bin/splunk enable boot-start -user $SPLUNK_LINUXUSER
-#test3
 
