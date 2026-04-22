1e306a204e7281ae935df26828d58b4e29678065
gyorisziszi <gyorisziszi@gmail.com>
2026-04-22 12:03:49 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,12 @@ yum install wget -y
 yum install nano -y
 yun install bind-utils -y
 yum install net-tools -y
+
+#install and enable ssh
+sudo yum install -y openssh-server
+sudo systemctl start sshd
+sudo systemctl enable sshd
+
 mkdir /opt/git
 cd /opt/git
 git clone https://github.com/gyorisziszi/splunk.git
