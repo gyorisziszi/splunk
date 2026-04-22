4fd6e92d7ed0230bd195504edd5e4a9447e69aa4
gyorisziszi <gyorisziszi@gmail.com>
2026-04-22 18:42:40 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,9 +10,5 @@ sudo yum install -y openssh-server
 sudo systemctl start sshd
 sudo systemctl enable sshd
 
-mkdir /opt/git
-cd /opt/git
-git clone https://github.com/gyorisziszi/splunk.git
-cd /opt/git/splunk
 #bash install_splunk_ent.sh
 #bash install_splunk_uf.sh
