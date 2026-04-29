e8b577a3cb31f4e75c32e6366cfa9abdd3d48981
gyorisziszi <gyorisziszi@gmail.com>
2026-04-29 15:34:16 +0200

    added htop

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,8 @@ yum install wget -y
 yum install nano -y
 yum install bind-utils -y
 yum install net-tools -y
+yum install epel-release -y
+yum install htop -y
 
 #install and enable ssh
 sudo yum install -y openssh-server
