eb8e526dfb5c9cda84bbc98165229ab933b9063b
gyorisziszi <gyorisziszi@gmail.com>
2026-05-19 15:32:23 +0200

    Update upgrade_splunk_ent.sh

--- upgrade_splunk_ent.sh
+++ upgrade_splunk_ent.sh
@@ -30,3 +30,7 @@ cd /opt
 rpm -Uvh $FULL_PATH
 
 chown -R splunk:splunk /opt/splunk
+
+sudo -u $SPLUNK_LINUXUSER $SPLUNK_HOME/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd admin123
+
+$SPLUNK_HOME/bin/splunk enable boot-start -user $SPLUNK_LINUXUSER
